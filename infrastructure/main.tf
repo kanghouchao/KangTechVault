f1666b3666c97aad65eba6429c2509bc793814e2
Kang Houchao <hchkang8710@gmail.com>
2024-09-01 00:01:26 +0900

    init main.tf

---
+++ infrastructure/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0cab37bd176bb80d3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
