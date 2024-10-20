a2f73a719083fd01d50ce8195db704e665a6a365
Kang Houchao <hchkang8710@gmail.com>
2024-10-20 22:28:45 +0900

    init

---
+++ backend/src/main/java/com/kang/demonstration/DemonstrationApplication.java
@@ -0,0 +1,17 @@
+package com.kang.demonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author kanghouchao
+ */
+@SpringBootApplication
+public class DemonstrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemonstrationApplication.class, args);
+    }
+
+}
