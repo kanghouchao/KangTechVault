4fba9935dfa7414fd1e31a590e2db55c474f4be6
Kang Houchao <hchkang8710@gmail.com>
2024-12-15 22:16:45 +0900

    名前を変わる、エラーを改善する

--- backend/src/main/java/com/kang/demonstration/auth/unit/TokenProvider.java
+++ backend/src/main/java/com/kang/demonstration/auth/unit/TokenProvider.java
@@ -5,97 +5,71 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.time.Instant;
 import java.util.Date;
-import java.util.Map;
+import java.util.List;
 import java.util.function.Function;
 
 @Component
-public class JwtUtil {
+public class TokenProvider {
+
+    @Value(value = "${login-token.expiration.time-millis}")
+    private Long expirationTimeMillis = 1000 * 60 * 60 * 24L;
+
+    private static final String USERNAME = "username";
+    private static final String AUTHORITIES = "authorities";
     private static final String SECRET_KEY = "c7f1d7e68c56a91e92f3b1c4a3979a93f639946b034ea87b96e5d65eb07888b3";
     private final SecretKey key;
 
     private final JwtParser parser;
 
-    public JwtUtil() {
+    public TokenProvider() {
         this.key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
         this.parser = Jwts.parser()
             .verifyWith(key)
             .build();
     }
 
-    /**
-     * 生成 JWT
-     *
-     * @param claims               自定义声明
-     * @param expirationTimeMillis 过期时间（毫秒）
-     * @return JWT 字符串
-     */
-    public String generateToken(Map<String, String> claims, long expirationTimeMillis) {
+    public String generateToken(String username, List<String> authorities) {
         Instant now = Instant.now();
-
         return Jwts.builder()
-            .claims().add(claims).and()
+            .claims()
+            .add(USERNAME, username)
+            .add(AUTHORITIES, authorities)
+            .and()
             .issuedAt(Date.from(now))
             .expiration(Date.from(now.plusMillis(expirationTimeMillis)))
             .signWith(key)
             .compact();
     }
 
-    /**
-     * 提取 JWT 中的某个字段
-     *
-     * @param token          JWT 字符串
-     * @param claimsResolver 提取函数
-     * @return 提取结果
-     */
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = parseToken(token).getBody();
+        final Claims claims = parseToken(token).getPayload();
         return claimsResolver.apply(claims);
     }
 
-    /**
-     * 从 JWT 中提取用户名
-     *
-     * @param token JWT 字符串
-     * @return 用户名
-     */
     public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
+        return extractClaim(token, claims -> claims.get(USERNAME, String.class));
     }
 
-    /**
-     * 验证 JWT 是否有效
-     *
-     * @param token    JWT 字符串
-     * @param username 用户详情
-     * @return 是否有效
-     */
     public boolean isTokenValid(String token, String username) {
         String extractedUsername = extractUsername(token);
         return username.equals(extractedUsername) && !isTokenExpired(token);
     }
 
-    /**
-     * 检查 JWT 是否过期
-     *
-     * @param token JWT 字符串
-     * @return 是否过期
-     */
     private boolean isTokenExpired(String token) {
         return extractClaim(token, Claims::getExpiration).before(new Date());
     }
 
-    /**
-     * 解析 JWT
-     *
-     * @param token JWT 字符串
-     * @return 解析后的 Jws<Claims> 对象
-     */
     private Jws<Claims> parseToken(String token) {
         return parser.parseSignedClaims(token);
+    }
+
+    public long getExpirationTimeMillis() {
+        return expirationTimeMillis;
     }
 }
